cc4d2ab0650fa6b3312ba1aedf669b7c6d1b92c1
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-10 15:16:09 -0300

    refactor: rename Equipamento to Equipment and update property names to English

---
+++ GestaoDeEquipamentos.ConsoleApp/Domain/Equipment.cs
@@ -0,0 +1,9 @@
+class Equipment
+{
+  public int id;
+  public string? name;
+  public string? manufacturer;
+  public decimal purchasePrice;
+  public DateTime manufactoringDate;
+
+}
--- GestaoDeEquipamentos.ConsoleApp/Dominio/Equipamento.cs
+++
@@ -1,9 +0,0 @@
-class Equipamento
-{
-  public int numeroIdentificacao;
-  public string nome;
-  public string fabricante;
-  public decimal precoAquisicao;
-  public DateTime dataFabricacao;
-
-}
