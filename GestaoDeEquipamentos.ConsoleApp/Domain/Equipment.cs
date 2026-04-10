c33305759d6e49f81d5a3e339860aeee408bd92a
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-10 16:57:50 -0300

    fix: add nullable reference types to the class fields

--- GestaoDeEquipamentos.ConsoleApp/Domain/Equipment.cs
+++ GestaoDeEquipamentos.ConsoleApp/Domain/Equipment.cs
@@ -2,7 +2,7 @@ namespace GestaoDeEquipamentos.ConsoleApp.Domain;
 
 class Equipment
 {
-  public string id;
+  public string? id;
   public string? name;
   public string? manufacturer;
   public decimal purchasePrice;
