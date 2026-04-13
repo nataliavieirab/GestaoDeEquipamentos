4dfd5f49b79730954c6abf5e794a4bb2f59e5577
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-13 12:35:36 -0300

    feat: add basic ticket management feature

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,23 +1,57 @@
 ﻿using GestaoDeEquipamentos.ConsoleApp.Presentation;
 
+ScreenUtils screenUtils = new ScreenUtils();
 EquipmentScreen equipmentScreen = new EquipmentScreen();
+TicketScreen ticketScreen = new TicketScreen();
 
 while (true)
 {
 
-  string menuOption = equipmentScreen.GetMainMenuOption();
+  string mainMenuOption = screenUtils.GetMainMenuOption();
 
-  if (menuOption == "S")
+  if (mainMenuOption == "S")
   {
     Console.Clear();
     break;
   }
 
-  if (menuOption == "1") equipmentScreen.Register();
+  if (mainMenuOption == "1")
+  {
+    string menuOption = equipmentScreen.GetMenuOption();
 
-  else if (menuOption == "2") equipmentScreen.Edit();
+    if (menuOption == "S")
+    {
+      Console.Clear();
+      break;
+    }
 
-  else if (menuOption == "3") equipmentScreen.Delete();
+    if (menuOption == "1") equipmentScreen.Register();
 
-  else if (menuOption == "4") equipmentScreen.ShowAll();
+    else if (menuOption == "2") equipmentScreen.Edit();
+
+    else if (menuOption == "3") equipmentScreen.Delete();
+
+    else if (menuOption == "4") equipmentScreen.ShowAll();
+  }
+
+  else if (mainMenuOption == "2")
+  {
+    string menuOption = ticketScreen.GetMenuOption();
+
+    if (menuOption == "S")
+    {
+      Console.Clear();
+      break;
+    }
+
+    // if (menuOption == "1") ticketScreen.Register();
+
+    // else if (menuOption == "2") ticketScreen.Edit();
+
+    // else if (menuOption == "3") ticketScreen.Delete();
+
+    // else if (menuOption == "4") ticketScreen.ShowAll();
+
+  }
 }
+
