91b6748c3dabc442a7d6a62d46f948ead62fc616
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-04-13 16:09:54 -0300

    feat: add create method

--- GestaoDeEquipamentos.ConsoleApp/Infra/TicketRepository.cs
+++ GestaoDeEquipamentos.ConsoleApp/Infra/TicketRepository.cs
@@ -1,4 +1,18 @@
+using System.Security.Cryptography;
+using GestaoDeEquipamentos.ConsoleApp.Domain;
+
 class TicketRepository
 {
+  public List<Ticket> tickets = new List<Ticket>();
+  public void Create(Ticket newTicket)
+  {
+    newTicket.id = Convert
+    .ToHexString(RandomNumberGenerator.GetBytes(20))
+    .ToLower()
+    .Substring(0, 7);
+
+    tickets.Add(newTicket);
+  }
+
 
 }
